6a4e5c7f0d09db5dfbe6fd7e6dd54138644776da
Ruben Andres Reyes Cuellar <rareyesc19960629@gmail.com>
2024-11-07 18:32:26 -0500

    Se agrega interfaz para index de compañias y manejo de eventos

---
+++ src/views/indexAdmin.vue
@@ -0,0 +1,9 @@
+<template>
+    <h4>Index Admin</h4>
+</template>
+<script>  
+    export default ({
+        name: 'indexAdmin',
+    })
+</script>
+    
---
+++ src/views/indexUser.vue
@@ -0,0 +1,8 @@
+<template>
+    <h4>Index Admin</h4>
+</template>
+<script>  
+    export default ({
+        name: 'indexUser',
+    })
+</script>
